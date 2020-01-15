e84996e75a6c921e663ccca97f177c38188af64e
Gabriel <ggbr12.neg@gmail>
2020-01-15 16:28:38 -0300

    up viewer and add logo

--- web/www/resources/views/dashboard.blade.php
+++ web/www/resources/views/dashboard.blade.php
@@ -3,23 +3,23 @@
     <div id="app">
         <div class="columns">
             <div class="column">
-                <h1> Servers</h1>
+                <h1 class="title is-white"> Servers</h1>
             </div>
         </div>
        <div class="columns">
 
             <div class="column" v-for="server in servers">
-                <h2>Server @{{server.name}}</h2>
                 <div class="card" >
                     <div class="card-content">
+                        <h2 class="title">@{{server.name}}</h2>
+                        <a class="button is-dark" target="_blanck" v-bind:href="server.url"> Access server</a>
+                        <br>
+                        <br>
                         <embed  v-bind:src="server.url + '/api/v1/badge.svg?chart=system.cpu&alarm=10min_cpu_usage&refresh=auto'" type="image/svg+xml" height="20"/>
                         <embed  v-bind:src="server.url + '/api/v1/badge.svg?chart=system.ram&alarm=ram_in_use&refresh=auto'" type="image/svg+xml" height="20"/>
                         <embed  v-bind:src="server.url + '/api/v1/badge.svg?chart=system.active_processes&alarm=active_processes_limit&refresh=auto'" type="image/svg+xml" height="20"/>
                         <embed bind:src="server.url + '/api/v1/badge.svg?chart=disk_space._&alarm=disk_space_usage&refresh=auto'" type="image/svg+xml" height="20"/>
-
-                        <br>
-                        Load
-                        <br>
+                        <hr>
                         <embed v-bind:src="server.url + '/api/v1/badge.svg?chart=system.load&alarm=load_average_1&refresh=auto'" type="image/svg+xml" height="20"/>
                         <embed v-bind:src="server.url + '/api/v1/badge.svg?chart=system.load&alarm=load_average_5&refresh=auto'" type="image/svg+xml" height="20"/>
                         <embed v-bind:src="server.url + '/api/v1/badge.svg?chart=system.load&alarm=load_average_15&refresh=auto'" type="image/svg+xml" height="20"/>
